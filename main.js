6bc4b8eb21536881ea94d0ea74ea94c7abfaa5df
Andrey Dev <andreysamaniego1@gmail.com>
2026-06-06 17:19:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+﻿<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="theme-color" content="#030303" />
+    <title>Drey Vieira Barbearia</title>
+    <link rel="icon" type="image/png" href="/logo.png" />
+    <link rel="stylesheet" href="/styles.css" />
+  </head>
+  <body>
+    <div id="app"></div>
+    <script type="module" src="/main.js"></script>
+  </body>
+</html>
